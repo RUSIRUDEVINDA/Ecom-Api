ea0fdb92c47f54a55e8b88d28c557c7a585d5b33
RUSIRUDEVINDA <rusirud49@gmail.com>
2026-06-15 13:09:59 +0530

    correct sqlc paths to existing adapter/migration folders

--- internal/adapteres/postgresql/migration/00001_create_product.sql
+++ internal/adapteres/postgresql/migration/00001_create_product.sql
@@ -1,8 +1,8 @@
 -- +goose Up
-CREATE TABLE IF NOT EXSISTS products(
+CREATE TABLE IF NOT EXISTS products(
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    price_in_cents INTEGER NOT NULL CHECK (price_in_cents >= 0)
+    price_in_cents INTEGER NOT NULL CHECK (price_in_cents >= 0),
     quantity INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
